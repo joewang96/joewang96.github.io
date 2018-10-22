1df063ca5c1c8a7059a8d89dbbf780079476fe51
Joseph Wang <wang.jo@husky.neu.edu>
2018-10-21 22:30:43 -0400

    transition effects

--- src/composers/WrappedNavFooter.js
+++ src/composers/WrappedNavFooter.js
@@ -3,13 +3,31 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
 class WrappedNavFooter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loaded: true,
+      });
+    }, 100);
+  }
+
   render() {
     return (
-      <>
+      <div
+        id="container"
+        className={this.state.loaded ? 'preload' : 'preload preset'}
+      >
         <Nav />
         {this.props.children}
         <Footer />
-      </>
+      </div>
     );
   }
 }
