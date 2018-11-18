af8461a7b33de8aaf57bdd8f7b1fa98b579c5310
Joseph Wang <wang.jo@husky.neu.edu>
2018-11-18 02:03:50 -0500

    change default offset for scroll function

--- src/lib/scroll.js
+++ src/lib/scroll.js
@@ -1,5 +1,5 @@
 export const scrollTo = (selector, delta) => {
-  const offset = delta || 0;
+  const offset = delta || 40;
   const yDist = document.querySelector(selector).offsetTop;
   window.scroll({ top: yDist - offset, left: 0, behavior: 'smooth' });
 };
